5f3f6c0fd0c26c3f70b03c5e3acb1af6e3aa8ebc
hz-php <sia041081@gmail.com>
2024-09-21 21:21:51 +0300

    add request and add new method

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <x-guest-layout>
-                    <form method="POST" action="{{ route('register') }}">
+                    <form method="POST" action="{{ route('users.store') }}">
                         @csrf
 
                         <!-- First_name -->
@@ -56,12 +56,8 @@
                         </div>
 
                         <div class="flex items-center justify-end mt-4">
-                            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
-                                {{ __('Already registered?') }}
-                            </a>
-
                             <x-primary-button class="ms-4">
-                                {{ __('Register') }}
+                                {{ __('Save') }}
                             </x-primary-button>
                         </div>
                     </form>
